b601e8a15f3b7b17f04a432de51316afb23baf4a
loutouk <louisboursier@hotmail.fr>
2019-02-03 19:36:36 +0100

    add conda yml env file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # KerasDeepLearning
+
+Using Anaconda prompt, load the environment file: conda env create -f deepl_env.yml
+
+Activate it: activate deepl_env.yml
+
+Play it: python path/to/my_super_file.py
--- Self_Organizing_Maps/som.py
+++ Self_Organizing_Maps/som.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Credit_Card_Applications.csv')
+dataset = pd.read_csv('C:\\Users\\Louis\\Documents\\Computing\\AI\\Perso\\Deep_Learning_A_Z\\Self_Organizing_Maps\\Credit_Card_Applications.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
