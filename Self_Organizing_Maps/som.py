4d6c268661a442fa1b273334684503dc6fea659f
loutouk <louisboursier@hotmail.fr>
2019-02-03 20:09:43 +0100

    comments on SOM

--- Self_Organizing_Maps/som.py
+++ Self_Organizing_Maps/som.py
@@ -17,19 +17,33 @@ X = sc.fit_transform(X)
 
 # Training the SOM
 from minisom import MiniSom
+# The size of the SOM is arbitrary, we choose 10*10
+# The input_len corresponds to the numbers of features of our dataset
+# Sigma is distance of the different neighborhood in the grid
 som = MiniSom(x = 10, y = 10, input_len = 15, sigma = 1.0, learning_rate = 0.5)
 som.random_weights_init(X)
 som.train_random(data = X, num_iteration = 100)
 
 # Visualizing the results
 from pylab import bone, pcolor, colorbar, plot, show
+# Create the window
 bone()
+# Add the matrix of all the mean inter-neuron distances for all the wining nodes
+# We take the transpose of this matrix with .T
 pcolor(som.distance_map().T)
+# Legend for the MID
+# Frauds are rare and different from other operations, so they have bigger MID
+# Because outline wining nodes correspond to the frauds, we can spot them with their high MID
 colorbar()
+# Add markers: red circles for customer who did not get banking card approval
+# And green square for those who have been approved
 markers = ['o', 's']
 colors = ['r', 'g']
 for i, x in enumerate(X):
+	# Get the winning nodes
     w = som.winner(x)
+    # Place the marker at the middle of the square
+    # And add a marker according to its approval success
     plot(w[0] + 0.5,
          w[1] + 0.5,
          markers[y[i]],
@@ -40,6 +54,12 @@ for i, x in enumerate(X):
 show()
 
 # Finding the frauds
-mappings = som.win_map(X)
+mappings = som.win_map(X) # dict: coordinate on the matrix => list of costumer at this position
+# Now we want to take the frauds identified on the graphed matrix (high MID) 
+# And map their coordinate to their id thanks to the mappings variable
+# TODO change the following coordinate to match the one of the MID nodes (look at the scale on the gui)
+# Concacenate all the results
 frauds = np.concatenate((mappings[(8,1)], mappings[(6,8)]), axis = 0)
+# We scaled the values before, so now we unscalle them
 frauds = sc.inverse_transform(frauds)
+print(frauds)
